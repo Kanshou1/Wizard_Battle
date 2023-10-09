288d7bdc0fd079e4a64341ac981298239d2a1a54
Kanshou1 <73129248+Kanshou1@users.noreply.github.com>
2023-10-09 11:31:24 -0300

    wizard battle

--- Program.py
+++ Program.py
@@ -1,4 +1,6 @@
-# import actors
+import random
+import time
+
 from actors import Wizard, Creature
 
 
@@ -22,21 +24,35 @@ def game_loop():
         Creature('Dragon', 50),
         Creature('Evil Wizard', 1000)
     ]
-    print(creatures)
+    # print(creatures)
     hero = Wizard('Gandalf', 75)
 
     while True:
 
+        active_creature = random.choice(creatures)
+        print('A {} of level {} has appeared from a dark and foggy forest...'
+              .format(active_creature.name, active_creature.level))
+        print()
+
         cmd = input('Do you [a]ttack, [r]un away, or [l]ook around?')
         if cmd == 'a':
-            print('attack')
+            if hero.attack(active_creature):
+                creatures.remove(active_creature)
+            else:
+                print("The wizard runs and hides taking time to recover...")
+                time.sleep(5)
+                print("The wizard returns revitalized!")
         elif cmd == 'r':
-            print('run away')
+            print('The wizard has become unsure of his power and flees!')
         elif cmd == 'l':
-            print('look around')
+            print('the wizard {} takes in the surroundings and sees:'.format(hero.name))
+            for c in creatures:
+                print(' * A {} of level {}'.format(c.name, c.level))
         else:
             print('Ok, exiting game... bye!')
             break
+
+        print()
 
 
 if __name__ == '__main__':
--- actors.py
+++ actors.py
@@ -1,13 +1,34 @@
+import random
+
+
 class Wizard:
-    def __int__(self, name, level):
+    def __init__(self, name, level):
         self.name = name
         self.level = level
 
+    def attack(self, creature):
+        print('The wizard {} attacks {}!'.format(
+            self.name, creature.name
+        ))
+
+        my_roll = random.randint(1, 12) * self.level
+        creature_roll = random.randint(1, 12) * creature.level
+
+        print("you roll {}...".format(my_roll))
+        print("{} rolls {}...".format(creature.name, creature_roll))
+        if my_roll >= creature_roll:
+            print("The wizard has handily triumphed over {}".format(creature.name))
+            return True
+        else:
+            print("The wizard has been DEFEATED ")
+            return False
+
 
 class Creature:
-    def __int__(self, name, the_level):
+    def __init__(self, name, the_level):
         self.name = name
         self.level = the_level
+
 
     def __repr__(self):
         return "Creature: {} of level {}".format(
